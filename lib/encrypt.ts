6a29890d8a62c57d867e70134b5b10bbbb885c67
Matthew Morrison <matthew@mattymo.dev>
2026-03-06 00:24:20 -0500

    fix sending a string instead of buffer

--- lib/encrypt.ts
+++ lib/encrypt.ts
@@ -1,7 +1,7 @@
 import { publicEncrypt } from "crypto";
 
-export function encrypt(message: string): Buffer {
+export function encrypt(message: string): string {
     const publicKey = process.env.ENCRYPTION_PUBLIC_KEY
     if (publicKey == null) throw new Error("No public key");
-    return publicEncrypt(Buffer.from(publicKey, 'base64'), message);
+    return publicEncrypt(Buffer.from(publicKey, 'base64'), message).toString('base64');
 }
