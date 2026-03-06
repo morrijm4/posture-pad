af33adade81f7a23cd7592cfa046f4d4f25ab1cc
Matthew Morrison <matthew@mattymo.dev>
2026-03-06 00:25:47 -0500

    fix build

--- scripts/encrypt.ts
+++ scripts/encrypt.ts
@@ -3,7 +3,7 @@ import { encrypt } from '../lib/encrypt.ts';
 async function main() {
     process.loadEnvFile('.env');
     for (const msg of process.argv.slice(2)) {
-        console.log(encrypt(msg).toString('base64'));
+        console.log(encrypt(msg));
     }
 }
 
