d68a2a2f9644eb45edb1ab7f5edf98a67a6cc711
Matthew Morrison <matthew@mattymo.dev>
2026-03-06 19:20:26 -0500

    control the video time by the clientX mouse position

--- components/homepage/3d-before-after-gallery.tsx
+++ components/homepage/3d-before-after-gallery.tsx
@@ -1,9 +1,41 @@
+'use client';
+
+import { useEffect, useRef } from "react"
+
 export default function ThreeDBeforeAfterGallery() {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        const video = videoRef.current;
+        if (!video) return;
+
+        let lastX = 0;
+        let ticking = false;
+
+        function updateVideoTime() {
+            if (video && !isNaN(video.duration)) {
+                video.currentTime = (lastX / window.innerWidth) * video.duration;
+            }
+            ticking = false;
+        }
+
+
+        const controller = new AbortController()
+        window.addEventListener('mousemove', (e) => {
+            lastX = e.clientX;
+
+            if (!ticking) {
+                window.requestAnimationFrame(updateVideoTime);
+                ticking = true;
+            }
+        }, { signal: controller.signal });
+        return () => controller.abort();
+    }, []);
+
     return (
         <div className="relative w-full rounded-xl overflow-hidden">
             <video
-                autoPlay
-                loop
+                ref={videoRef}
                 muted
                 playsInline
                 preload="auto"
