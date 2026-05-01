2a238fb3fee8abd6bb2cc8b5cd435a44184d7570
Matthew Morrison <matthew@mattymo.dev>
2026-05-01 11:31:16 -0400

    https

--- apps/www/app/calibrate/page.tsx
+++ apps/www/app/calibrate/page.tsx
@@ -110,7 +110,7 @@ export default function Page() {
 
         if (i === stages.length - 1) {
             try {
-                const res = await fetch("http://pp.mattymo.dev/train", {
+                const res = await fetch("https://pp.mattymo.dev/train", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
